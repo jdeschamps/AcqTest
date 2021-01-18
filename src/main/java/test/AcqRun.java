93620cbb9ea6e64a4dd72a5671cf281195757afe
Joran Deschamps <jdeschamps@users.noreply.github.com>
2021-01-18 21:56:46 +0100

    use old sequencesettings approach

--- src/main/java/test/AcqRun.java
+++ src/main/java/test/AcqRun.java
@@ -2,12 +2,13 @@ package main.java.test;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 import org.micromanager.Studio;
 import org.micromanager.acquisition.AcquisitionManager;
 import org.micromanager.acquisition.SequenceSettings;
-import org.micromanager.acquisition.SequenceSettings.Builder;
+//import org.micromanager.acquisition.SequenceSettings.Builder;
 import org.micromanager.data.Datastore;
 
 
@@ -51,7 +52,7 @@ public class AcqRun implements Runnable{
 
 	
 	private void runSnap(String timeSuffix) {
-		Builder seqBuilder = new SequenceSettings.Builder();
+		/*Builder seqBuilder = new SequenceSettings.Builder();
 		seqBuilder.save(true);
 		seqBuilder.timeFirst(true);
 		seqBuilder.usePositionList(false);
@@ -61,10 +62,21 @@ public class AcqRun implements Runnable{
 		seqBuilder.intervalMs(0);
 		seqBuilder.shouldDisplayImages(true);
 		seqBuilder.useFrames(true);
+		*/
+		
+		SequenceSettings seq = new SequenceSettings();
+		seq.save = true;
+		seq.timeFirst = true;
+		seq.usePositionList = false;
+		seq.root = path;
+		seq.prefix = NAME+timeSuffix+SNAP;
+		seq.numFrames = 1;
+		seq.intervalMs = 0;
+		seq.shouldDisplayImages = true;
 		
 		// run acquisition
 		AcquisitionManager acqManager = studio.acquisitions();
-		acqManager.setAcquisitionSettings(seqBuilder.build());
+		acqManager.setAcquisitionSettings(seq);
 		Datastore store = acqManager.runAcquisition();
 		
 		// to mimic what we have in the other plugin for user interruption
@@ -87,7 +99,7 @@ public class AcqRun implements Runnable{
 
 	
 	private void runTime(String timeSuffix) {
-		Builder seqBuilder = new SequenceSettings.Builder();
+		/*Builder seqBuilder = new SequenceSettings.Builder();
 		seqBuilder.save(true);
 		seqBuilder.timeFirst(true);
 		seqBuilder.usePositionList(false);
@@ -96,11 +108,21 @@ public class AcqRun implements Runnable{
 		seqBuilder.numFrames(150);
 		seqBuilder.intervalMs(0);
 		seqBuilder.shouldDisplayImages(true);
-		seqBuilder.useFrames(true);
+		seqBuilder.useFrames(true);*/
+		
+		SequenceSettings seq = new SequenceSettings();
+		seq.save = true;
+		seq.timeFirst = true;
+		seq.usePositionList = false;
+		seq.root = path;
+		seq.prefix = NAME+timeSuffix+TIME;
+		seq.numFrames = 150;
+		seq.intervalMs = 0;
+		seq.shouldDisplayImages = true;
 		
 		// runs acquisition
 		AcquisitionManager acqManager = studio.acquisitions();		
-		acqManager.setAcquisitionSettings(seqBuilder.build());
+		acqManager.setAcquisitionSettings(seq);
 		Datastore store = acqManager.runAcquisitionNonblocking();
 
 		// to mimic what we have in the other plugin for user interruption
@@ -123,7 +145,7 @@ public class AcqRun implements Runnable{
 
 	
 	private void runZStack(String timeSuffix) {
-		Builder seqBuilder = new SequenceSettings.Builder();
+		/*Builder seqBuilder = new SequenceSettings.Builder();
 		seqBuilder.save(true);
 		seqBuilder.slicesFirst(true);
 		seqBuilder.usePositionList(false);
@@ -136,11 +158,30 @@ public class AcqRun implements Runnable{
 		seqBuilder.relativeZSlice(true);
 		seqBuilder.sliceZBottomUm(-1.);
 		seqBuilder.sliceZStepUm(0.1);
-		seqBuilder.sliceZTopUm(1.);
+		seqBuilder.sliceZTopUm(1.);*/
+		
+		SequenceSettings seq = new SequenceSettings();
+		seq.save = true;
+		seq.timeFirst = false;
+		seq.slicesFirst = true;
+		seq.usePositionList = false;
+		seq.root = path;
+		seq.prefix = NAME+timeSuffix+ZSTACK;
+		seq.numFrames = 1;
+		seq.intervalMs = 0;
+		seq.shouldDisplayImages = true;
+		
+		ArrayList<Double> slices = new ArrayList<Double>();
+		for(int i=-2; i<=2; i++) {
+			slices.add(new Double(i));
+		}
+		
+		seq.slices = slices;
+		
 		
 		// run acquisition
 		AcquisitionManager acqManager = studio.acquisitions();
-		acqManager.setAcquisitionSettings(seqBuilder.build());
+		acqManager.setAcquisitionSettings(seq);
 		Datastore store = acqManager.runAcquisitionNonblocking();
 
 		
